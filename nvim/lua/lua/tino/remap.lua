13060ff9a5b4670fad5a1815c43a85eb5d758e65
cvonsteg <cvonstegmann@gmail.com>
2023-04-15 18:16:52 +0100

    extra mappings

--- nvim/lua/lua/tino/remap.lua
+++ nvim/lua/lua/tino/remap.lua
@@ -1,6 +1,13 @@
 local options = { noremap = true }
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<ESC>", options)
+-- window split
+vim.keymap.set('n', '<leader>.', vim.cmd.sp)
+vim.keymap.set('n', '<leader>/', vim.cmd.vsp)
+-- buffers
+vim.keymap.set('n', 'bn', vim.cmd.bnext)
+vim.keymap.set('n', 'bp', vim.cmd.bprev)
+vim.keymap.set('n', 'bd', vim.cmd.bdelete)
 -- move visual blocks up and down
 vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv'")
 vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv'")
