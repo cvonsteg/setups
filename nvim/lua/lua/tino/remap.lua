fa81717192d0cf3be303473015fd3590c4419d64
cvonsteg <cvonstegmann@gmail.com>
2023-04-17 21:09:35 +0100

    save remap

--- nvim/lua/lua/tino/remap.lua
+++ nvim/lua/lua/tino/remap.lua
@@ -1,6 +1,8 @@
 local options = { noremap = true }
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<ESC>", options)
+-- quick save
+vim.keymap.set('n', '<C-s>', vim.cmd.w)
 -- window split
 vim.keymap.set('n', '<leader>.', vim.cmd.sp)
 vim.keymap.set('n', '<leader>/', vim.cmd.vsp)
@@ -9,8 +11,8 @@ vim.keymap.set('n', 'bn', vim.cmd.bnext)
 vim.keymap.set('n', 'bp', vim.cmd.bprev)
 vim.keymap.set('n', 'bd', vim.cmd.bdelete)
 -- move visual blocks up and down
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv'")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv'")
 -- when searching keep cursor centered
+vim.keymap.set('v', 'J', ":m '>+1<cr>gv=gv'")
+vim.keymap.set('v', 'k', ":m '<-2<cr>gv=gv'")
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
